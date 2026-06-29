09506589ef16386d68b1b717d0989b85c0fb5c14
ULTRATELECOM PROVEDORA DE INTERNET <ultrat.site@gmail.com>
2026-06-29 09:02:56 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,18 @@
+import os
 from flask import Flask
 from routes import init_routes
 from dotenv import load_dotenv
 
+# Carrega as variáveis do arquivo .env
 load_dotenv()
+
 app = Flask(__name__)
-app.secret_key = 'sua_chave_secreta_aqui'
+
+# Usa a chave que definimos no arquivo .env para maior segurança
+app.secret_key = os.getenv('FLASK_SECRET_KEY')
 
 init_routes(app)
 
 if __name__ == "__main__":
+    # Mantém na porta 5000 interna do container
     app.run(host="0.0.0.0", port=5000)
